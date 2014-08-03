c84ee9ef9d75aba937880c8d900f21b081fe9c54
Amy Hanlon <amyehanlon@gmail.com>
2014-08-03 14:25:10 -0400

    add skeleton phonebook

---
+++ 2-code-skeleton/phonebook.py
@@ -0,0 +1,54 @@
+def create_phonebook(phonebook_name):
+    """Create a new phonebook.
+
+    Args:
+        phonebook_name (str): the name of the phonebook to create
+    """
+    pass
+
+
+def add_entry(name, number, phonebook_name):
+    """Add a new name and number to the given phonebook.
+
+    Args:
+        name (str): name of the entry to add
+        number (str): phone number of the entry to add
+        phonebook_name (str): name of the phonebook
+    """
+    pass
+
+
+def update_entry(name, new_number, phonebook_name):
+    """Update an entry with the given name in the given phonebook
+    with the new phone bumber.
+
+    Args:
+        name (str): name of the entry to update
+        new_number (str): new phone number of the entry
+        phonebook_name (str): name of the phonebook
+    """
+    pass
+
+
+def lookup_name(name, phonebook_name):
+    """Look up an entry by name in the given phonebook.
+
+    Args:
+        name (str): name to look up
+        phonebook_name (str): name of the phonebook
+    """
+    pass
+
+
+def lookup_number(number, phonebook_name):
+    """Look up an entry by number in the given phonebook.
+
+    Args:
+        number (str): phone number to look up
+        phonebook_name (str): name of the phonebook
+    """
+    pass
+
+
+if __name__ == '__main__':
+    pass
